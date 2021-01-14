247d46cab1eb5bb9da4dbd13ad81eca4de307c68
kembek <zevs.312.777@gmail.com>
2021-01-14 11:34:15 +0600

    chore: eslint and prettier were added

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,30 +1,33 @@
-import React from "react";
-import Link from "next/link";
-import styles from "../../styles/Header.module.css";
+import Link from 'next/link';
+import React from 'react';
+
+import styles from '../../styles/Header.module.css';
 
 interface IHeaderLinkProps {
-    href: string;
+  href: string;
 }
 
 const HeaderLink: React.FC<IHeaderLinkProps> = ({ href, children }) => (
-    <span className={styles.HeaderLink}>
-        <Link href={href}>{children}</Link>
-    </span>
+  <span className={styles.HeaderLink}>
+    <Link href={href}>{children}</Link>
+  </span>
 );
 
 interface IHeaderProps {
-    // ...
+  // ...
 }
 
 const Header: React.FC<IHeaderProps> = () => {
-    return <header className={styles.Header}>
-        <nav>
-            <HeaderLink href="#">Main Page</HeaderLink>
-            <HeaderLink href="#">Second Page</HeaderLink>
-            <HeaderLink href="#">Third Page</HeaderLink>
-            <HeaderLink href="#">Fourth Page</HeaderLink>
-        </nav>
-    </header>;
-}
+  return (
+    <header className={styles.Header}>
+      <nav>
+        <HeaderLink href="#">Main Page</HeaderLink>
+        <HeaderLink href="#">Second Page</HeaderLink>
+        <HeaderLink href="#">Third Page</HeaderLink>
+        <HeaderLink href="#">Fourth Page</HeaderLink>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import '../styles/globals.css'
-import React from "react";
-import Header from "../components/header";
-import styles from "../styles/Page.module.css";
+import '../styles/globals.css';
+
+import React from 'react';
+
+import Header from '../components/header';
+import styles from '../styles/Page.module.css';
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Header />
-    <div className={styles.Page}>
-      <Component {...pageProps} />
-    </div>
-  </>
+  return (
+    <>
+      <Header />
+      <div className={styles.Page}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
